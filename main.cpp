1e65618476fde969322332c26d13dbdf4b4e0da7
Tomasz Wasielewski <s24280@szuflandia.pjwstk.edu.pl>
2021-11-07 12:27:21 +0100

    drugi kod kalkulatora

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 int main() {
-	std::cout << "Podaj 3 liczby: << std::endl;
-	int a,b,c;
-	std::cin >> a >> b >> c;
-	std::cout << "Srednia z nich wynosi: " << (a+b+c)/3;
+	std::cout << "Podaj 4 liczby: << std::endl;
+	int a,b,c,d;
+	std::cin >> a >> b >> c >> d;
+	std::cout << "Srednia z nich wynosi: " << (a+b+c+d)/4;
 	return 0;
 }
